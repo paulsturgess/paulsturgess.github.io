4340e0e079d2af6a8cd6cd57df1e2b6f19fb3302
Paul Sturgess <paul.sturgess@hopin.to>
2022-11-06 20:03:17 +0000

    New one page tailwindcss version

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontSize: {
+      sm: '1rem',
+      base: '1.25rem',
+      xl: '1.5rem',
+      '2xl': '1.75rem',
+    }
+  },
+  plugins: [],
+  darkMode: 'class',
+}

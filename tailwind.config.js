d1d24ce28841591e4ed6916c09d7a38e05e0ec29
Paul Sturgess <paul.sturgess@hopin.to>
2022-11-06 20:55:57 +0000

    Mobile layout improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,7 @@
 
 module.exports = {
   content: ["./index.html"],
-  theme: {
-    fontSize: {
-      sm: '1rem',
-      base: '1.25rem',
-      xl: '1.5rem',
-      '2xl': '1.75rem',
-    }
-  },
+  theme: {},
   plugins: [],
   darkMode: 'class',
 }
